dacfd995df566277cedc32bc1e735594c4183813
Mohit Varma <varmamohit122@gmail.com>
2021-08-10 22:13:08 +0530

    Create Graph Theory

--- Graph Theory/Floyd Warshall/Floyd Warshall.cpp
+++ Graph Theory/Floyd Warshall/Floyd Warshall.cpp
@@ -1,12 +1,12 @@
-Floyd-Warshall with Path:-
-    
+Floyd - Warshall with Path: -
+
 int dist[N][N], p[N][N];
 
 void FloydWarshall(int n, int source, int destination)
 {
-    for(int k = 1; k <= n; k++) {
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= n; j++) {
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
                 if (dist[i][j] > dist[i][k] + dist[k][j]) {
                     dist[i][j] = dist[i][k] + dist[k][j];
                     p[i][j] = p[i][k];
@@ -14,19 +14,19 @@ void FloydWarshall(int n, int source, int destination)
             }
         }
     }
-    
+
     if (p[source][destination] == -1) {
         cout << "No Path" << '\n';
         return;
     }
-    
+
     vector<int> path;
     path.push_back(source);
     while (source != destination) {
         source = p[source][destination];
         path.push_back(source);
     }
-    
-    for (auto x : path) 
+
+    for (auto x : path)
         cout << x << " ";
 }
