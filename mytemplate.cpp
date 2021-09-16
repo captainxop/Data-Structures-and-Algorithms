e58b99c8d36ebaff13c094d1c3cfe02659ae13d4
Mohit Varma <70433964+captainxop@users.noreply.github.com>
2021-09-16 17:01:09 +0530

    Update mytemplate.cpp

--- mytemplate.cpp
+++ mytemplate.cpp
@@ -1,4 +1,7 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
 
 #define pb push_back
@@ -9,7 +12,7 @@ using namespace std;
 #define ss second
 
 #define rep(i, a, b) for(int i = a; i < b; i++)
-#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define repR(i, a, b) for(int i = a; i >= b; --i)
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define sz(x) (int)(x).size()
@@ -33,6 +36,7 @@ typedef pair<ll, ll> pll;
 #define mod 1000000007
 #define ps(x,y) fixed << setprecision(y) << x
 #define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 void solve()
 {
