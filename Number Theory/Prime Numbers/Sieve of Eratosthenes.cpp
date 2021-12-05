f7e19d53f8e17e162683c8d52332869d39b2216a
Mohit Varma <varmamohit122@gmail.com>
2021-12-06 00:45:56 +0530

    Updated Sieve of Eratosthenes.cpp

--- Number Theory/Prime Numbers/Sieve of Eratosthenes.cpp
+++ Number Theory/Prime Numbers/Sieve of Eratosthenes.cpp
@@ -1,26 +1,14 @@
-const int N = 2e5;
-bool isPrime[N + 10];
-vector<int> primes;
+const int MAXN = 2e6 + 10;
+bool isPrime[MAXN];
 
-void sieve()
+void sieve(ll n)
 {
-    for(int i = 0; i <= N; i++) {
-        isPrime[i] = true;
-    }
-    
-    isPrime[0] = isPrime[1] = false;
-    
-    for (int i = 2; i * i <= N; i++) {
-        if (isPrime[i] && i * i <= N) {
-            for (int j = i * i; j <= N; j += i) {
-                isPrime[j] = false;
+    isPrime[0] = isPrime[1] = 1;
+    for (ll i = 2; i * i <= n; i++) {
+        if (!isPrime[i]) {
+            for (ll j = i * i; j <= n; j += i) {
+                isPrime[j] = 1;
             }
         }
-    }
-    
-    primes.pb(2);
-    for (int i = 3; i <= N; i += 2) {
-        if (isPrime[i]) 
-            primes.pb(i);
     }
 }
