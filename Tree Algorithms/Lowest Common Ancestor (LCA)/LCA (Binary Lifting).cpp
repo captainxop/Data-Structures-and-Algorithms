53a7539e01b0a8930fdc98eb521089eb9c215606
Mohit Varma <varmamohit122@gmail.com>
2021-12-06 03:27:10 +0530

    Updated LCA (Binary Lifting).cpp

--- Tree Algorithms/Lowest Common Ancestor (LCA)/LCA (Binary Lifting).cpp
+++ Tree Algorithms/Lowest Common Ancestor (LCA)/LCA (Binary Lifting).cpp
@@ -1,45 +1,38 @@
-const int MAXN = 2e5 + 10;
+const int MAXN = (int)2e5 + 10;
 const int LOG = 20;
 vector<int> adj[MAXN];
 int up[MAXN][LOG];
-int lv[MAXN];
+int depth[MAXN];
 
-void dfs(int u, int p)
+void dfs(int u, int p = -1)
 {
-	lv[u] = lv[p] + 1;
-	up[u][0] = p;
-
 	for (auto v : adj[u]) {
-		if (v != p) {
-			dfs(v, u);
+		if (v == p) continue;
+
+		up[v][0] = u;
+		depth[v] = depth[u] + 1;
+		for (int j = 1; j < LOG; j++) {
+			up[v][j] = up[up[v][j - 1]][j - 1];
 		}
+
+		dfs(v, u);
 	}
 }
 
-void precompute()
+int get_lca(int u, int v)
 {
-	for (int j = 1; j < LOG; j++) {
-		for (int i = 0; i < n; i++) {
-			up[i][j] = up[up[i][j - 1]][j - 1];
-		}
-	}
-}
-
-int getLCA(int u, int v)
-{
-	if (lv[u] < lv[v])
+	if (depth[u] < depth[v]) {
 		swap(u, v);
+	}
 
-	int diff = lv[u] - lv[v];
-
+	int k = depth[u] - depth[v];
 	for (int j = LOG - 1; j >= 0; j--) {
-		if (diff & (1 << j)) {
+		if (k & (1 << j)) {
 			u = up[u][j];
 		}
 	}
 
-	if (u == v)
-		return u;
+	if (u == v) return u;
 
 	for (int j = LOG - 1; j >= 0; j--) {
 		if (up[u][j] != up[v][j]) {
@@ -51,7 +44,7 @@ int getLCA(int u, int v)
 	return up[u][0];
 }
 
-int dist(int u, int v)
+int get_distance(int u, int v)
 {
-	return lv[u] + lv[v] - 2 * lv[getLCA(u, v)];
+	return depth[u] + depth[v] - 2 * depth[get_lca(u, v)];
 }
