eff7ec126fc35f0546882e99e03ef39370b441dd
Mohit Varma <varmamohit122@gmail.com>
2022-01-28 15:52:50 +0530

    Created Topological Sorting.cpp

---
+++ Graph Theory/Topological Sorting/Topological Sorting.cpp
@@ -0,0 +1,38 @@
+vector<bool> visited(300010, 0), stk(300010, 0);
+vector<int> toposort; // Stores the Topological Order
+
+bool check_cycle(int u)
+{
+	visited[u] = 1;
+	stk[u] = 1;
+
+	for (auto v : adj[u]) {
+		if (!visited[v]) {
+			bool cycle = check_cycle(v);
+			if (cycle) {
+				return true;
+			}
+		}
+		else if (stk[v]) {
+			return true;
+		}
+	}
+
+	stk[u] = 0;
+	return false;
+}
+
+void dfs(int u)
+{
+	visited[u] = 1;
+
+	for (auto v : adj[u]) {
+		if (!visited[v]) {
+			dfs(v);
+		}
+	}
+
+	toposort.pb(u);
+}
+
+reverse(toposort.begin(), toposort.end());
